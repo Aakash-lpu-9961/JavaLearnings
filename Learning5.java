d705873d6762035fca599db4c9c077d9462f0192
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2022-01-04 13:16:32 +0530

    Test Commit

---
+++ Learning5.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Learning5 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World5");
+    }
+}
