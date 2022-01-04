5d09a38dfc01c6d61c6812c787898089f1c9d857
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2022-01-04 12:00:43 +0530

    First Commit

---
+++ Learning1.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Learning1 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World1");
+
+        int value1=56;
+        int value2=65;
+        int value3=99;
+        int value4= value1+value2+value3;
+        System.out.println("Result of addition is:");
+
+    }
+}
---
+++ Learning2.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Learning2 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World2");
+    }
+}
