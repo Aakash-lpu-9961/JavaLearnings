33cfa7357e37ad0fcdcbd629e22edc855cc1ef1b
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2022-06-04 23:47:57 +0530

    JDBC MySQL Connectivity L19 VVI

---
+++ Learning19.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.sql.*;
+
+public class Learning19 {
+    public static void main(String[] args) throws SQLException {
+
+        //  STEP -01 : Create a Connection
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/aakashdb", "root", "@Aakash9961");
+
+        //  STEP - 02 : Create Statement/Query
+        Statement stmt=con.createStatement();
+//        String s="INSERT INTO Student VALUES(117, 'Nitin', 20, 'M', 'Delhi', 7554493570)";
+        String m = "SELECT SID, SNAME, SAGE, SCITY, SPHN  from student";
+
+        //  STEP - 03 : Execute Statement/Query
+//             stmt.execute(s);
+        ResultSet rs =stmt.executeQuery(m);
+
+        while (rs.next())
+        {
+            int sid=rs.getInt("SID");
+            String name=rs.getString("SNAME");
+            int sage=rs.getInt("SAGE");
+//            String sgen=rs.getString("SGEN");
+            String scity=rs.getString("SCITY");
+            long sphn=rs.getLong("SPHN");
+
+            System.out.println(sid+"  "+name+"  "+sage+"  "+scity+"  "+sphn);
+        }
+
+
+        //4) Close Connection
+        con.close();
+
+        System.out.println("Query executed....");
+
+    }
+}
+
+//Reference : SQL Tutorial 14: JDBC | Java Database Connectivity with MySQL
+// Channel Name - SDET-QA Automation Techie?
